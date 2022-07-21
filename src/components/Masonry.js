09495fcf8923af7735ac4a9095cd5aabaff0c523
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2022-07-21 10:10:18 -0600

    Update Masonry.js

--- src/components/Masonry.js
+++ src/components/Masonry.js
@@ -1,25 +1,32 @@
 import React, { useState, useEffect } from "react";
 import classes from "./Masonry.module.css";
-import Brick from './Brick'
+import Brick from "./Brick";
 
 const Masonry = (props) => {
   const [apiResponse, setApiResponse] = useState([]);
-  let imageUrl = "https://longwoodgardens.org/sites/default/files/highlight_images/137439.jpg"
-useEffect(() => {
-    
-  fetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
-    .then((response) => {
-      const data = response.json();
-      return data;
-    })
-    .then((data) => {
-        console.log(data.data)
-      setApiResponse(data.data);
-    });}, [])
+  let imageUrl =
+    "https://longwoodgardens.org/sites/default/files/highlight_images/137439.jpg";
+  useEffect(() => {
+    fetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
+      .then((response) => {
+        const data = response.json();
+        return data;
+      })
+      .then((data) => {
+        console.log(data.data);
+        setApiResponse(data.data);
+      });
+  }, []);
   return (
     <div className={classes.masonryContainer}>
       {apiResponse?.map((item) => {
-        return <Brick className={classes.item} imageTitle={`${item.Year}: ${item.Population}`} imageUrl={imageUrl}/>;
+        return (
+          <Brick
+            className={classes.item}
+            imageTitle={`${item.Year}: ${item.Population}`}
+            imageUrl={imageUrl}
+          />
+        );
       })}
     </div>
   );
