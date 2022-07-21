b1f4ea9af5a1dd30419bbd66b37f21cfd310ad94
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2022-07-20 20:43:34 -0600

    styled navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,28 @@ const NavBar = (props) => {
   return (
     <nav className={classes.nav}>
       <div className={classes.infoContainer}>
-        <div>
-          <img src="" alt="" /> <p>My unsplash</p>
+        <div className={classes.logoContainer}>
+          <div className={classes.iconContainer}>
+            <div className={classes.rectangleSm}></div>
+            <div className={classes.rectangleLg}></div>
+          </div>{" "}
+          <div className={classes.titleContainer}>
+            <p className={classes.title}>My unsplash</p>
+          <p className={classes.subtitle}>devchallenges.io</p>
+          </div>
+          
         </div>
         <form>
-          <input type="text" placeholder="Search By Title" className={classes.search}/>
+          <input
+            type="text"
+            placeholder="Search By Title"
+            className={classes.search}
+          />
         </form>
       </div>
-      <button type="button" className={classes.btn}>Add a photo</button>
+      <button type="button" className={classes.btn}>
+        Add a photo
+      </button>
     </nav>
   );
 };
